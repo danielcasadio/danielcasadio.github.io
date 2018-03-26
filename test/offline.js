954714a8738a11cd61933b68e534a16fa74376b0
danielcasadio <danielcasadio@gmail.com>
2018-03-26 02:51:19 -0300

    Spaceships added

--- test/offline.js
+++ test/offline.js
@@ -1,11 +1,16 @@
 ﻿{
-	"version": 1522039296,
+	"version": 1522043405,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
 		"images/sprite-sheet0.png",
+		"images/ship-sheet0.png",
+		"images/sky.png",
+		"images/enemybig-sheet0.png",
+		"images/shoot-sheet0.png",
+		"images/sprite2-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
